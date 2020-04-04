8b1b10ea3114be6a5c72967f8a0faa4e5ca4e2ad
Khandker <khandker.islam46@gmail.com>
2020-04-04 19:05:29 -0400

    finished http client

--- http-client.js
+++ http-client.js
@@ -1,2 +1,10 @@
 const http = require('http');
 const fs = require('fs');
+
+const url = process.argv[2];
+http.get(url,(res) => {
+    res.setEncoding('utf8')
+    res.on('data',(data)=>{
+        process.stdout.write(data +'\n');
+    })
+});
