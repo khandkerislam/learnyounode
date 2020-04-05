2396a7969ffe44dbd8c6986aae4643dfbbbd099c
Khandker <khandker.islam46@gmail.com>
2020-04-05 01:58:30 -0400

    on http collect

--- http-collect.js
+++ http-collect.js
@@ -3,19 +3,9 @@ const url = process.argv[2];
 
 http.get(url,(res)=>{
     const {statusCode} = res;
-    let error;
-    if(statusCode !== 200){
-        error = new Error('Request Failed.\n' + `Status Code: ${statusCode}`);
-    } 
-    if(error){
-        console.error(error.message);
-        res.resume();
-        return;
-    }
-
     res.setEncoding('utf8');
-    let rawData = '';
-    res.on('event',(chunk) => { rawData += chunk;});
+    let rawData = statusCode;
+    res.on('data',(chunk) => { rawData += chunk;});
     res.on('end', () => {
         try {
             console.log(rawData);
