626e373a2551e81c0248a84029573bc3269b2680
Khandker <khandker.islam46@gmail.com>
2020-04-05 17:22:55 -0400

    What have we done all day

--- http-collect.js
+++ http-collect.js
@@ -1,18 +1,11 @@
 const http = require('http');
 const url = process.argv[2];
+let rawData = '';
 
 http.get(url,(res)=>{
-    const {statusCode} = res;
-    res.setEncoding('utf8');
-    let rawData = statusCode;
     res.on('data',(chunk) => { rawData += chunk;});
     res.on('end', () => {
-        try {
-            console.log(rawData);
-        } catch(e){
-            console.error(e.message);
-        }
+        console.log(rawData.length);
+        console.log(rawData);
     });
-}).on('error', (e) => {
-    console.error(`Got error: ${e.message}`);
-})
+});
