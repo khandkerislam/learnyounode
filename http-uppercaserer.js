82c3e2505da1c651550cd324419287d8dc7c4950
Khandker <khandker.islam46@gmail.com>
2020-04-05 19:07:41 -0400

    Penultimate

---
+++ http-uppercaserer.js
@@ -0,0 +1,10 @@
+const http = require('http');
+const map = require('through2-map');
+
+http.createServer((req,res)=>{
+    if (req.method != 'POST')
+        return res.end('send me a POST\n')
+    req.pipe(map(function (chunk) {
+        return chunk.toString().toUpperCase()
+    })).pipe(res)
+}).listen(Number(process.argv[2]));
